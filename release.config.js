411d82236d06fa5ea291d8b5f53ff6bb2449a92b
Elad Chen <3628913+eladchen@users.noreply.github.com>
2020-09-25 01:43:07 +0300

    fix: another major

--- release.config.js
+++ release.config.js
@@ -1,35 +1,3 @@
-/**
- * semantic-release docs: https://semantic-release.gitbook.io/semantic-release
- *
- * Plugins:
- *
- *  @semantic-release/commit-analyzer
- *  Determine whether a release should be made, and its type.
- *  https://github.com/semantic-release/commit-analyzer
- *
- *  @semantic-release/release-notes-generator
- *  Generate release notes, which can be read in the context parameter of other plugins.
- *  For example, the changelog plugin uses when writing the changes to a file.
- *  https://github.com/semantic-release/release-notes-generator
- *
- *  @semantic-release/changelog
- *  Write release notes to a file (Notes are passed in plugins context parameter).
- *  https://github.com/semantic-release/changelog
- *
- *  @semantic-release/exec
- *  Run commands during arbitrary release steps (instead of writing a plugin).
- *  String interpolation is available, using: https://github.com/semantic-release/git/blob/master/lib/prepare.js
- *  https://github.com/semantic-release/exec
- *
- *  @semantic-release/git
- *  Perform commit and push of files generated during a release. (tricky with protected branches)
- *  https://github.com/semantic-release/git
- *
- *  @semantic-release/github
- *  Create a Github release, and comment on relevant pull-requests / issues
- *  https://github.com/semantic-release/github
- */
-
 const plugin = (name, options) => [name, options];
 
 const changeLogFilePath = "CHANGELOG.md";
@@ -55,39 +23,18 @@ module.exports = {
       changelogFile: changeLogFilePath,
     }),
 
-    plugin("@semantic-release/exec", {
-      prepareCmd: cmd(`node release-scripts/create-build-info.js`, [
-        `--file-path ${buildInfoFilePath}`,
-        `--build-version \${nextRelease.version}`,
-      ]),
+    "/Users/elad/dev/github/semantic-release-test/release-scripts/semantic-release.js",
 
-      publishCmd: cmd(`node release-scripts/publish-docker-image.js`, [
-        `--image-tag \${nextRelease.version}`,
-        `--image-name ${process.env.DOCKER_IMAGE_NAME}`,
+    // plugin("@semantic-release/exec", {
+    //   verifyConditionsCmd: "false",
 
-        `--registry ${process.env.DOCKER_REGISTRY}`,
-        `--registry-user-name ${process.env.DOCKER_REGISTRY_USER_NAME}`,
-      ]),
-    }),
+    //   prepareCmd: "false",
 
-    plugin("@semantic-release/exec", {
-      publishCmd: cmd(`node release-scripts/update-k8s-deployment.js`, [
-        `--deployment-image ${process.env.DOCKER_IMAGE_NAME}:\${nextRelease.version}`,
-        `--deployment-name ${process.env.K8S_DEPLOYMENT_NAME}`,
-        `--deployment-host ${process.env.APP_HOST}`,
-      ]),
-    }),
-
-    plugin("@semantic-release/exec", {
-      publishCmd: cmd(`node release-scripts/validate-deployment.js`, [`--expected-version \${nextRelease.version}`]),
-    }),
+    //   publishCmd: "false",
+    // }),
 
     plugin("@semantic-release/git", {
       assets: ["package.json", changeLogFilePath],
-    }),
-
-    plugin("@semantic-release/github", {
-      assets: [{ path: buildInfoFilePath }],
     }),
   ],
 };
